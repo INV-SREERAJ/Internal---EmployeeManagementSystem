cb464d7927f2a6e3dcee600cda121feb8ab24a36
INV-SREERAJ <sreeraj.r@innovaturelabs.com>
2026-07-27 16:01:35 +0530

    remove role repo

--- src/EmployeeManagementSystem.DataAccess/Entities/Employee.cs
+++ src/EmployeeManagementSystem.DataAccess/Entities/Employee.cs
@@ -9,7 +9,7 @@ namespace EmployeeManagementSystem.DataAccess.Entities
     {
         public int Id { get; set; }
         public string EmployeeCode { get; set; } = string.Empty;
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
